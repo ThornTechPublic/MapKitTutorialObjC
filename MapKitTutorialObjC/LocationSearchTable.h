eaa14d65e1f725bf98a02cfc3ff63bbb9f3750f2
Robert Chen <robert.chen@thorntechnologies.com>
2016-01-19 14:42:12 -0500

    Using a proper @import statement

--- MapKitTutorialObjC/LocationSearchTable.h
+++ MapKitTutorialObjC/LocationSearchTable.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <MapKit/MapKit.h>
+@import MapKit;
 #import "ViewController.h"
 
 @interface LocationSearchTable : UITableViewController <UISearchResultsUpdating>
--- MapKitTutorialObjC/ViewController.h
+++ MapKitTutorialObjC/ViewController.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <MapKit/MapKit.h>
+@import MapKit;
 
 @protocol HandleMapSearch <NSObject>
 - (void)dropPinZoomIn:(MKPlacemark *)placemark;
