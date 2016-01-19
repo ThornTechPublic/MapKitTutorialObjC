4af9f6ce47f39d3912d5e6d3fd073a4f0316d03a
Robert Chen <robert.chen@thorntechnologies.com>
2016-01-19 11:21:07 -0500

    Set up the Location Manager

--- MapKitTutorialObjC/ViewController.h
+++ MapKitTutorialObjC/ViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CLLocationManagerDelegate>
 
 
 @end
--- MapKitTutorialObjC/ViewController.m
+++ MapKitTutorialObjC/ViewController.m
@@ -14,14 +14,35 @@
 
 @implementation ViewController
 
+CLLocationManager *locationManager;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.delegate = self;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    [locationManager requestLocation];
+    [locationManager requestWhenInUseAuthorization];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status
+{
+    if (status == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        [locationManager requestLocation];
+    }
 }
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    NSLog(@"error: %@", error);
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+     didUpdateLocations:(NSArray<CLLocation *> *)locations
+{
+    CLLocation *location = [locations firstObject];
+    NSLog(@"location: %@", location);
+}
+
 
 @end
